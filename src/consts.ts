765318d24e26ed915a94d0154934745a3935c70f
Jackson <jsingleton12321@yahoo.com>
2025-01-26 23:27:17 -0600

    initial commit

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,12 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "ASTROFUNK";
+export const SITE_DESCRIPTION = "こんばんは";
+
+export const ASTROFUNK = ((s) => s.raw)`
+ ______  ______  ______ ______  ______  ______ __  __  __   __  __  __
+/\  __ \/\  ___\/\__  _/\  == \/\  __ \/\  ___/\ \/\ \/\ "-.\ \/\ \/ /
+\ \  __ \ \___  \/_/\ \\ \  __<\ \ \/\ \ \  __\ \ \_\ \ \ \-.  \ \  _"-.
+ \ \_\ \_\/\_____\ \ \_\\ \_\ \_\ \_____\ \_\  \ \_____\ \_\\"\_\ \_\ \_\
+  \/_/\/_/\/_____/  \/_/ \/_/ /_/\/_____/\/_/   \/_____/\/_/ \/_/\/_/\/_/`[0];
