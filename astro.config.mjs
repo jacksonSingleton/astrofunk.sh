9c8767ab30887d511abf349f3d0d32dc14a8bc69
Jackson <jsingleton12321@yahoo.com>
2025-10-25 09:08:51 -0500

    api udpates fixed

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
+	output: 'hybrid',
+	adapter: netlify(),
 	integrations: [mdx(), sitemap()],
 });
