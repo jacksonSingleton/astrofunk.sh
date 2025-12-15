f53b93ba2b96e316c2341202048e6150fa5cb2a1
Jackson <jsingleton12321@yahoo.com>
2025-12-15 16:55:36 -0600

    yea

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import remarkCallout from '@r4ai/remark-callout';
 
 import netlify from '@astrojs/netlify';
 
@@ -10,4 +11,7 @@ export default defineConfig({
   site: 'https://example.com',
   integrations: [mdx(), sitemap()],
   adapter: netlify(),
+    markdown: {
+        remarkPlugins: [remarkCallout],
+    }
 });
